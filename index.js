d4416904c8bc7ac1dbd913e33558e471256454bc
mukthar <muktharmanaf28@gmail.com>
2025-01-17 15:35:02 +0530

    reply for the messages

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const dotenv = require("dotenv");
 
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent,] });
+
+   
+   
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+client.on(Events.MessageCreate,(message)=>{
+
+    if(!message.author.bot){
+            message.reply( {content:'hi from bot'})
+        
+    }
+})
+
+
+client.login(process.env.DISCORD_TOKEN);
